93d99170f1315914296685f1b7d5a3ef2e51f148
pavel kozhevnikov <pavel.k@dot-tech.ru>
2021-11-22 20:56:02 +0300

    Написал док стринг и передал конфиги в класс QueryData для route GET /candidates

--- proxy/main.py
+++ proxy/main.py
@@ -1,26 +1,24 @@
 from flask import Flask, jsonify
 from services import QueriesToServer
+from config import QueryToServerConfig
 
 
 app = Flask(__name__)
 
+
 @app.route('/candidates', methods=['GET'])
 def get_candidates():
     """
-    TODO
+    Делает запрос на server и возвращает полную информацию о кандидатах
     """
-    # сломал этот роут и поменял нейминг
-    # TODO:  в качестве задания предлагаю починить
-    query = QueriesToServer('http://localhost:8089')
+    query = QueriesToServer(QueryToServerConfig)
     candidates_data, validation_errors = query.get_data()
     results = jsonify(candidates_data)
     if validation_errors is None: # вот эту строчку изменять нельзя
         return (results, 200)
     else:
-        return (results, 200)
+        return (results, 206)
     
 
 if __name__ == '__main__':
     app.run(host='127.1.1.1', port=8100, debug=True)
-
-
